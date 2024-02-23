4b689a5097b5b279743ff1ea2b1d32c823ba7bf8
Elena Krismer <70535771+elena-krismer@users.noreply.github.com>
2024-02-23 14:15:50 +0100

    fix

--- dspa_viz_py/barcode.py
+++ dspa_viz_py/barcode.py
@@ -44,13 +44,10 @@ class BarcodePlotter:
 
     def update_len_vector(
         self,
-        len_vector: np.ndarray,
         start: int,
         end: int,
         log2FC: float,
         qvalue: float,
-        qvalue_cutoff: float,
-        log2FC_cutoff: float
     ) -> None:
         """
         Update the length vector based on specified criteria.
@@ -65,60 +62,45 @@ class BarcodePlotter:
         """
         log2FC = 0 if np.isinf(log2FC) else log2FC
         for i in range(start, end):
-            if qvalue < qvalue_cutoff and abs(log2FC) > log2FC_cutoff:
-                len_vector[i] = np.mean([len_vector[i], log2FC]) if len_vector[i] != 0 else log2FC
+            if self.type == "dynamic":
+                if qvalue < self.qvalue_cutoff and abs(log2FC)> self.log2FC_cutoff:
+                    self.len_vector[i] = np.mean([self.len_vector[i], log2FC]) if self.len_vector[i] != 0 else log2FC
+                else:
+                    self.len_vector_detected[i] = 1
+            else:
+                score = -np.log10(qvalue)+np.abs(log2FC)
+                self.len_vector[i] = np.mean([self.len_vector[i], score]) if self.len_vector[i] != 0 else score
+    
+    def prepare_data(
+            self,
+            prot:str,
+            LiP_df:pd.DataFrame
+        ):
+        self.aa_seq = self.proteins_in_file_fasta_dict[prot].seq
+        
+        # Sort the dataframe by start
+        LiP_df[LiP_df.pg_protein_accessions == prot].sort_values('start', inplace=True)
 
-    def plot_barcode(
-        self,
-        prot: str,
-        aa_seq: str,
-        len_vector: np.ndarray,
-        title: Optional[str] = None,
-        cmap: Union[mpl.colors.Colormap, str] = "YlOrBr",
-        save_as_svg: bool = False
-    ) -> Union[str, plt.Figure]:
-        """
-        Plot barcode for the given protein.
+        # Create matrices for plot
+        self.dataframe_with_vector = pd.DataFrame(index=range(len(self.aa_seq)))
+        self.dataframe_with_vector_detected = pd.DataFrame(index=range(len(self.aa_seq)))
 
-        Parameters:
-        - prot (str): Protein name.
-        - aa_seq (str): Amino acid sequence.
-        - len_vector (np.ndarray): Length vector for plotting.
-        - title (str, optional): Title for the plot. Defaults to None.
-        - cmap (Union[mpl.colors.Colormap, str], optional): Colormap for the plot. Defaults to "YlOrBr".
-        - save_as_svg (bool, optional): Whether to save the plot as SVG. Defaults to False.
+        self.len_vector = np.zeros(len(self.aa_seq))
+        self.len_vector_detected = np.zeros(len(self.aa_seq))
 
-        Returns:
-        - Union[str, plt.Figure]: Either the name of the saved SVG image or the plot itself.
-        """
-        dataframe_with_vector = pd.DataFrame(index=range(len(aa_seq)))
-        dataframe_with_vector[0] = np.where(len_vector != 0, len_vector, np.nan)
-
-        fig, ax = plt.subplots(figsize=(6, 1))
-
-        ax.imshow(dataframe_with_vector.notna().T,
-                  cmap=cmap, aspect='auto', interpolation='nearest')
-
-        ax.imshow(dataframe_with_vector.values.reshape((1, -1)),
-                  cmap=cmap, aspect='auto', interpolation='nearest', vmin=0)
-
-        positions = np.arange(0, len(aa_seq), 100)
-        positions = np.append(positions, [1, len(aa_seq)])
-        positions = positions[positions != 0]
-
-        ax.set_xticks(positions)
-        ax.set_xticklabels(positions)
-        ax.set_yticklabels([])
-        ax.set_yticks([])
-        ax.set_title(title or prot, fontdict={'fontsize': 16, 'fontweight': 'medium', 'fontname': 'arial'})
-        sns.despine(left=True, right=True, top=True)
-
-        if save_as_svg:
-            plt.savefig(f'{prot}_structural_dynamics_barcode.svg', format='svg')
-            plt.close(fig)
-            return f'{prot}_structural_dynamics_barcode.svg'
-        else:
-            return fig
+        for _, row in LiP_df[LiP_df.pg_protein_accessions == prot].iterrows():
+                self.update_len_vector( 
+                    start=int(row['start']), 
+                    end=int(row['end']), 
+                    log2FC=row['diff'], 
+                    qvalue=row['adj_pval']
+                )
+        self.dataframe_with_vector[0] = np.where(self.len_vector != 0, self.len_vector, np.nan)
+        self.dataframe_with_vector_detected[0] = np.where(
+            (self.len_vector_detected == 1) & (self.len_vector == 0), 
+            0, 
+            np.nan
+        )     
 
     def plot_dynamics_barcode(
         self,
@@ -143,30 +125,57 @@ class BarcodePlotter:
         Returns:
         - Union[str, plt.Figure]: Either the name of the saved SVG image or the plot itself.
         """
-        if prot in self.organism_proteins:
-            aa_seq = self.proteins_in_file_fasta_dict[prot].seq
-            LiP_df = self.sort_dataframe_by_start(LiP_df[LiP_df.pg_protein_accessions == prot])
+        self.type = "dynamic"
 
-            dataframe_with_vector, len_vector = self.create_empty_matrices(aa_seq)
+        if prot not in self.organism_proteins:
+            print(f"{prot} not found")
+            return
+        
+        self.qvalue_cutoff = qvalue_cutoff
+        self.log2FC_cutoff = log2FC_cutoff
 
-            for _, row in LiP_df.iterrows():
-                 self.update_len_vector(len_vector=len_vector, 
-                    start=int(row['start']), 
-                    end=int(row['end']), 
-                    log2FC=row['diff'], 
-                    qvalue=row['adj_pval'],
-                    qvalue_cutoff=qvalue_cutoff, 
-                    log2FC_cutoff=log2FC_cutoff,
-                    )
+        self.prepare_data(prot=prot, LiP_df=LiP_df)
+            
+        dataframe_with_vector_barcode = pd.concat(
+                [self.dataframe_with_vector, self.dataframe_with_vector_detected], 
+                axis=1
+        )
+        dataframe_with_vector_barcode.columns = ['sig', 'detected']
 
-            return self.plot_barcode(
-                prot,
-                aa_seq,
-                len_vector,
-                title=f'{prot} - Structural Dynamics Barcode',
-                cmap=mpl.colors.LinearSegmentedColormap.from_list("", ["silver", "silver"]),
-                save_as_svg=save_as_svg
-            )
+        # set color maps
+        cmap_detected = mpl.colors.LinearSegmentedColormap.from_list("", ["silver", "silver"])
+        cmap = mpl.colors.LinearSegmentedColormap.from_list("", ["black", "black"])
+        cmap_sig = mpl.colors.LinearSegmentedColormap.from_list("", ["orange", "orange"])
+            
+        # Plotting
+        fig, ax = plt.subplots(figsize=(6, 1))
+
+        ax.imshow(dataframe_with_vector_barcode[['sig', 'detected']].notna().T,
+                    cmap=cmap, aspect='auto', interpolation='nearest')
+        ax.imshow(dataframe_with_vector_barcode[['sig']].values.reshape((1, -1)),
+                    cmap=cmap_sig, aspect='auto', interpolation='nearest', vmin=0)
+        ax.imshow(dataframe_with_vector_barcode[['detected']].values.reshape((1, -1)),
+                    cmap=cmap_detected, aspect='auto', interpolation='nearest')
+            
+        # Set x-axis ticks and labels
+        positions = np.arange(0, len(self.aa_seq), 100)
+        positions = np.append(positions, [1, len(self.aa_seq)])
+        positions = positions[positions != 0]
+
+        ax.set_xticks(positions)
+        ax.set_xticklabels(positions)
+        ax.set_yticklabels([])
+        ax.set_yticks([])
+
+        ax.set_title(prot, fontdict={'fontsize': 16, 'fontweight': 'medium', 'fontname': 'arial'})
+        sns.despine(left=True, right=True, top=True)
+
+        if save_as_svg:
+            plt.savefig(f'{prot}_structural_dynamics_barcode.svg', format='svg')
+            plt.close(fig)
+            return f'{prot}_structural_dynamics_barcode.svg'
+        else:
+            return fig
 
     def plot_residuelevel_barcode(
         self,
@@ -190,20 +199,38 @@ class BarcodePlotter:
         Returns:
         - Union[str, plt.Figure]: Either the name of the saved SVG image or the plot itself.
         """
-        if prot in self.organism_proteins:
-            aa_seq = self.proteins_in_file_fasta_dict[prot].seq
-            LiP_df = self.sort_dataframe_by_start(LiP_df[LiP_df.pg_protein_accessions == prot])
+        self.type = "residuelevel"
+        if prot not in self.organism_proteins:
+            print(f"{prot} not found")
+            return
+        
+        self.prepare_data(prot=prot, LiP_df=LiP_df) 
+        # set color maps
+        cmap = mpl.colors.LinearSegmentedColormap.from_list("", ["silver", "silver"])
+        # Plotting
+        fig, ax = plt.subplots(figsize=(6, 1))
 
-            dataframe_with_vector, len_vector = self.create_empty_matrices(aa_seq)
+        ax.imshow(
+            self.dataframe_with_vector.notna().T,
+            cmap=cmap, aspect='auto', interpolation='nearest')
+        
+        ax.imshow(self.dataframe_with_vector.values.reshape((1, -1)),
+            cmap="YlOrBr", aspect='auto', interpolation='nearest', vmin=0)
 
-            for _, row in LiP_df.iterrows():
-                self.update_len_vector(len_vector, int(row['start']), int(row['end']), row['diff'], row['adj_pval'], qvalue_cutoff, log2FC_cutoff)
+        # Set x-axis ticks and labels
+        positions = np.arange(0, len(self.aa_seq), 100)
+        positions = np.append(positions, [1, len(self.aa_seq)])
+        positions = positions[positions != 0]
 
-            return self.plot_barcode(
-                prot,
-                aa_seq,
-                len_vector,
-                title=f'{prot} - Residue-Level Dynamics Barcode',
-                cmap=mpl.colors.LinearSegmentedColormap.from_list("", ["silver", "silver"]),
-                save_as_svg=save_as_svg
-            )
+        ax.set_xticks(positions)
+        ax.set_xticklabels(positions)
+        ax.set_yticklabels([])
+        ax.set_yticks([])
+        ax.set_title(prot, fontdict={'fontsize': 16, 'fontweight': 'medium', 'fontname': 'arial'})
+        sns.despine(left=True, right=True, top=True)
+        if save_as_svg:
+            plt.savefig(f'{prot}_residuelevel_barcode.svg', format='svg')
+            plt.close(fig)
+            return f'{prot}_residuelevel_barcode.svg'
+        else:
+            return fig
